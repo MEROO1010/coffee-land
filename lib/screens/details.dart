a1d7edb2bd6f55f6d3e265b7b4cad96e34448c32
unknown <meroo1995saggaf@outlook.com>
2022-12-25 12:41:14 +0300

    flutter app coffee land

--- lib/screens/details.dart
+++ lib/screens/details.dart
@@ -1,6 +1,7 @@
 import 'package:coffee_land/screens/cappuccino.dart';
 import 'package:flutter/material.dart';
 import 'package:page_transition/page_transition.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main(List<String> args) {
   runApp(object1());
@@ -23,7 +24,9 @@ class _detailsState extends State<details> {
         ),
         Container(
           child: Image.asset('assets/images/img1.png'),
-          padding: EdgeInsets.only(left: 80, top: 120),
+          padding: EdgeInsets.only(bottom: 300),
+          width: 800,
+          height: 800,
         ),
         Container(
           child: TextButton(
@@ -44,7 +47,7 @@ class _detailsState extends State<details> {
               color: Color.fromRGBO(240, 183, 127, 1),
             ),
           ),
-          padding: EdgeInsets.only(left: 10, top: 60),
+          padding: EdgeInsets.only(left: 10, top: 40),
         ),
         Container(
           child: object1(),
@@ -64,10 +67,56 @@ class object1 extends StatefulWidget {
 class _object1State extends State<object1> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Container(
-      child: Image.asset('assets/images/Union 4.png'),
-      padding: EdgeInsets.only(left: 80, top: 500),
-    ));
+    return Stack(
+      children: [
+        MaterialApp(
+            home: Container(
+          child: Image.asset('assets/images/text.png'),
+          padding: EdgeInsets.only(left: 20, top: 300),
+        )),
+        RatingBar.builder(
+          initialRating: 4.5,
+          minRating: 1,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          itemCount: 5,
+          itemPadding: EdgeInsets.only(top: 500, left: 40),
+          itemSize: 18,
+          itemBuilder: (context, _) => Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (rating) {
+            print(rating);
+          },
+        ),
+        Container(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              'Buy Now',
+              style: TextStyle(
+                backgroundColor: Color.fromRGBO(152, 81, 68, 1),
+                color: Color.fromRGBO(237, 229, 218, 1),
+              ),
+            ),
+          ),
+          padding: EdgeInsets.only(top: 700, left: 200),
+        ),
+        Container(
+          child: TextButton(
+            onPressed: () {},
+            child: Text(
+              '10\$',
+              style: TextStyle(
+                backgroundColor: Color.fromRGBO(190, 83, 44, 1),
+                color: Color.fromRGBO(237, 229, 218, 1),
+              ),
+            ),
+          ),
+          padding: EdgeInsets.only(top: 700, left: 100),
+        )
+      ],
+    );
   }
 }
